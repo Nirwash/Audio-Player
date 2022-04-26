36d61646c6256643cdc90ba349616821101fdc74
Nirwash <nirwash@bk.ru>
2022-04-26 20:57:57 +0300

    добавление градиента в background верстка дизайна добавление Slider

--- app/src/main/java/com/nirwashh/android/playingaudio/MainActivity.kt
+++ app/src/main/java/com/nirwashh/android/playingaudio/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.nirwashh.android.playingaudio
 
+import android.annotation.SuppressLint
+import android.media.AudioManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.SeekBar
+import com.google.android.material.slider.Slider
 import com.nirwashh.android.playingaudio.databinding.ActivityMainBinding
 import kotlin.properties.Delegates
 
@@ -14,30 +19,89 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
+
         mediaPlayer = MediaPlayer.create(this, R.raw.stuff)
-        b.btnPlay.setOnClickListener { play() }
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        val maxVolue = (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)) / 15 * 100
+        //переводим в проценты для удобочитаемости, так как макс значение громкости 15
+
+        b.slider.valueTo = maxVolue.toFloat()
+        b.btnPlay.setOnClickListener { start() }
         b.btnPause.setOnClickListener { pause() }
-        if (mediaPlayer. > 0) {
-            val currentPosition = (mediaPlayer.duration / mediaPlayer.currentPosition) * 100
-            b.progressBar.apply {
-                visibility = View.VISIBLE
-                progress = currentPosition
+        b.btnStop.setOnClickListener { stop() }
+        b.btnBack.setOnClickListener { back() }
+        b.btnForward.setOnClickListener { forward() }
+        b.slider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+
+            @SuppressLint("RestrictedApi")
+            override fun onStartTrackingTouch(slider: Slider) {
             }
 
+            @SuppressLint("RestrictedApi")
+            override fun onStopTrackingTouch(slider: Slider) {
+            }
+        })
+        b.slider.addOnChangeListener { slider, value, fromUser ->
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, value.toInt(), 0)
         }
+
 
     }
 
-    private fun play() {
+
+    private fun start() {
         mediaPlayer.start()
-        b.btnPlay.visibility = View.GONE
-        b.btnPause.visibility = View.VISIBLE
+        if (b.btnPlay.isPressed) {
+            b.btnPlay.animate().scaleX(1.5F).scaleY(1.5F).duration = 500
+            b.btnPause.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnStop.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnBack.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnForward.animate().scaleX(1F).scaleY(1F).duration = 500
+        }
     }
 
     private fun pause() {
         mediaPlayer.pause()
-        b.btnPlay.visibility = View.VISIBLE
-        b.btnPause.visibility = View.GONE
+        if (b.btnPause.isPressed) {
+            b.btnPlay.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnPause.animate().scaleX(1.5F).scaleY(1.5F).duration = 500
+            b.btnStop.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnBack.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnForward.animate().scaleX(1F).scaleY(1F).duration = 500
+        }
+    }
+
+    private fun stop() {
+        mediaPlayer.pause()
+        if (b.btnStop.isPressed) {
+            b.btnPlay.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnPause.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnStop.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnBack.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnForward.animate().scaleX(1F).scaleY(1F).duration = 500
+        }
+    }
+
+    private fun back() {
+        mediaPlayer.pause()
+        if (b.btnBack.isPressed) {
+            b.btnPlay.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnPause.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnStop.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnBack.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnForward.animate().scaleX(1F).scaleY(1F).duration = 500
+        }
+    }
+
+    private fun forward() {
+        mediaPlayer.pause()
+        if (b.btnForward.isPressed) {
+            b.btnPlay.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnPause.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnStop.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnBack.animate().scaleX(1F).scaleY(1F).duration = 500
+            b.btnForward.animate().scaleX(1F).scaleY(1F).duration = 500
+        }
     }
 
 }
